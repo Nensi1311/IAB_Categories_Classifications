0e8faadde39bf98b322517c1ed70a743dd295cd1
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-12-20 22:39:05 +0530

    Delete Dataset_Unzip directory

--- Dataset_Unzip/Unzip.py
+++
@@ -1,8 +0,0 @@
-#import required modules
-import zipfile
-
-#First open the file in read mode
-unzip_files = zipfile.ZipFile('IAB.zip','r')
-
-#Now we will unzip and put those files in a directory called extracted_dir
-unzip_files.extractall("IAB")
