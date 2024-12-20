07d42be6c5e9913ce2303ac17e4ad379c2da3b79
Nensi Pansuriya <155217979+Nensi1311@users.noreply.github.com>
2024-12-20 22:37:35 +0530

    Update Unzip.py

--- Dataset_Unzip/Unzip.py
+++ Dataset_Unzip/Unzip.py
@@ -2,7 +2,7 @@
 import zipfile
 
 #First open the file in read mode
-unzip_files = zipfile.ZipFile('Final_IAB.zip','r')
+unzip_files = zipfile.ZipFile('IAB.zip','r')
 
 #Now we will unzip and put those files in a directory called extracted_dir
-unzip_files.extractall("Final_IAB")
+unzip_files.extractall("IAB")
